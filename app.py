39944bf85edc3895d613cb50adaca36a771b4db4
Robert Evans <robevans1412@gmail.com>
2023-12-12 17:52:40 +0000

    matched colour scheme

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ col1, col2 = st.columns(2)
 
 col1.markdown(
     """
-    <div style="font-family: 'PT Sans Narrow', sans-serif; background-color: #d8b6ff; font-size: 24px; padding: 10px; border-radius: 10px; text-align: center">
+    <div style="font-family: 'PT Sans Narrow', sans-serif; background-color: #db92b3; font-size: 24px; padding: 10px; border-radius: 10px; text-align: center">
     Your flower:
     </div>
     """, unsafe_allow_html=True)
@@ -81,7 +81,7 @@ col1.write("")
 
 col2.markdown(
     """
-    <div style="font-family: 'PT Sans Narrow', sans-serif; background-color: #d8b6ff; font-size: 24px; padding: 10px; border-radius: 10px; text-align: center">
+    <div style="font-family: 'PT Sans Narrow', sans-serif; background-color: #db92b3; font-size: 24px; padding: 10px; border-radius: 10px; text-align: center">
     Rose says:
     </div>
     """, unsafe_allow_html=True)
@@ -90,8 +90,6 @@ col2.write("")
 if img_file_buffer is None:
 
     col1.image(".streamlit/rose_hi.png", use_column_width=True)
-
-    #col2.image(".streamlit/rose_curious2_flipped.png", use_column_width=True)
 
 else:
 
@@ -154,99 +152,3 @@ else:
                         placeholder3.image(Image.open(".streamlit/rose_no.png"), use_column_width=True)
         else:
             placeholder2.markdown("**Oops**, something went wrong 😓 Please try again.")
-
-
-
-
-
-
-
-
-
-
-# Determine which image to display based on the prediction
-# if pred_class == "Tulip" and how_much_pink:
-#     image_path = ".streamlit/rose_no.png"
-# elif pred_class == "Sunflower":
-#     image_path = ".streamlit/rose_buh.png"
-# else:
-#     image_path = ".streamlit/rose_super_awesome.png"
-# # Display the appropriate image
-# st.image(Image.open(image_path), use_column_width=True)
-
-
-
-
-# # Change font and size
-# font_style = '''
-#      <style>
-#          body {
-#              font-family: 'PT Sans Narrow', sans-serif;
-#              font-size: 24px;
-#          }
-#      </style>
-#  '''
-# st.markdown(font_style, unsafe_allow_html=True)
-
-
-
-
-
-# c1, c2 = st.columns(2)
-
-# # Placeholder image for the right side
-# placeholder_image_path = ".streamlit/rose_hi.png"
-
-# # Display images side by side
-# c2.subheader("Rose says:")
-# c2.image(".streamlit/rose_curious2_flipped.png", use_column_width=True)
-
-# c1.subheader("Your flower")
-# c1.image(".streamlit/rose_hi.png", use_column_width=True)
-
-# Create two columns: one for the image and one for the text
-#image_column, text_column = st.columns([1, 2])
-
-# Display the image in the left column
-#image_column.image(".streamlit/rose.gif", use_column_width=True)
-
-# Set the style for the text in the right column
-#text_column.markdown(
-#     """
-#     <style>
-#         @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
-#         h1 {
-#             font-family: 'Homemade Apple', cursive;
-#             font-size: 48px;
-#             color: #000000;
-#             text-align: center;
-#         }
-#         p {
-#             font-family: 'Roboto', sans-serif;
-#             font-size: 24px;
-#             color: #333333;
-#             text-align: center;
-#         }
-#     </style>
-#     """,
-#     unsafe_allow_html=True
-# )
-
-# Display the title and subtitle in the right column
-#text_column.markdown(
-#     """
-#     <h1>
-#         The Rose Project!
-#     </h1>
-#     """,
-#     unsafe_allow_html=True
-# )
-
-# #text_column.markdown(
-#     """
-#     <p>
-#         An iris-istable flower classification project
-#     </p>
-#     """,
-#     unsafe_allow_html=True
-# )
